a4c6cf1f29dd1d772044aaff94e6e59ac460185d
SAYANTI MANNA <67185287+SayantiManna@users.noreply.github.com>
2020-10-12 00:42:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DGT-Skill India</title>
+  <link rel="shortcut icon" type="image/x-icon" href="DGT_fevicon.jpg">
+
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- ...........BOOTSTRAP CONTAINER LINK........... -->
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  <!-- ................BOOTSTRAP GRID LINK................. -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+  <!-- .................GOOGLE FONT................. -->
+ <link href="https://fonts.googleapis.com/css2?family=Diplomata+SC&family=Miltonian+Tattoo&display=swap" rel="stylesheet">
+
+
+  <style type="text/css">
+  	body{
+  		font-family: "Times New Roman";
+
+  	}
+  	.main{
+  		padding-left: 0px;
+  		padding-right:0px;
+      width:70%;
+      border:5px solid #ffad33;
+  	}
+  	.nav_bar{
+  		overflow:hidden;
+  		background-color: #ffad33;
+  		margin-bottom: 10px;
+  	}
+  	.nav_bar a {
+  		text-decoration: none;
+  		list-style-type: none;
+  		float:left;
+  		border-right: 1px solid white;
+  		padding: 0px 5px;
+  		display:block;
+  	}
+  	.nav_bar  a{
+  		color: #fff;
+  	}
+  	.content_img1{
+  		height:200px;
+  		width:300px;
+  		border:2px solid #ffad33;
+  		margin-left: 10px;
+  	}
+  	.content_img2{
+  		height:200px;
+  		width:300px;
+  		border:2px solid #ffad33;
+  		float:right;
+  		margin-right: 10px;
+  	}
+    .content_text{
+      background-color: #ffad33;
+      padding: 10px 10px 10px 10px;
+      margin: 20px 10px 15px 10px;
+      border-radius: 10px;
+      color: #fff;
+      text-align:justify;
+      font-size: 13px;
+    }
+
+    
+.cont_1{
+  height:200px;
+  padding: 10px 0px 0px 0px;
+  background: lightblue;
+  background-image: linear-gradient(115deg, #fed8b1 0% 37%, white 37% 68%, #FFFF99 68% 100%);
+}
+.div_1{
+  text-align: left;
+  padding-left: 15px;
+  display: inline-block;
+}
+.div_1 img{
+  height: 50px;
+  margin-right: 15px;
+}
+.div_1_txt{
+  margin-top:30px;
+  color: brown;
+  font-size: 20px;
+  text-align: center;
+  font-family: 'Diplomata SC', cursive;
+}
+.div_2{
+  display: inline-block;
+  padding-left: 5px;
+}
+.div_2 img{
+  margin-top: -80px;
+  height: 200px;
+  width: 300px;
+}
+.div_3{
+  float: right;
+  display: inline-block;
+}
+.div_3_txt{
+  font-weight: bold;
+  font-size: 18px;
+  text-align: center;
+  text-shadow: 0 0 5px #fff,0 0 10px red;
+}
+.div_3 img{
+  height: 150px;
+  width: 180px;
+}
+
+  </style>
+</head>
+<body>
+	<div class="container main">
+      <div class="cont_1">
+        <div class="div_1">
+          <img src="skillindia1.jpg">
+          <img src="dgt.png">
+          <img src="iti.png">
+          <p class="div_1_txt">PREPARE,<br> DON'T PANIC!</p>
+        </div>
+        <div class="div_2">
+          <img src="covid19_sym.png">
+        </div>
+        <div class="div_3">
+          <img src="covid.png">
+          <p class="div_3_txt">Covid-19</p>
+        </div>
+      </div>
+		<div class="nav_bar">
+			<a href="#">Home</a>
+			<a href="#">About Us</a>
+			<a href="#">Courses</a>
+			<a href="#">Contact Us</a>
+		</div>
+
+		<div class="content">
+			<div class="row">
+				<div class="col-6">
+					<img src="skillindia2.jpg" class="content_img1">
+				</div>
+				<div class="col-6">
+					<img src="dgt.jpg" class="content_img2">
+				</div>
+			</div>
+		</div>
+    <div class="content_second">
+      <p class="content_text">
+      The Craftmen Training Scheme (CTS) was introduced bt the Government of India in year 1950 to ensure a stedy flow of skilled workers in different trades for the domestic industry, to raise quantitatively and qualitatively the industrial production by systematic training, to reduce unemployment among the educated youth by providing them employable skills, to cultivate and nature a technical and industrial attitude ia the mind of younger generation. The scheme is the most important in the field of Vocational Training, has been shaping craftsmen to meet the existing as well as future manpower need, through the vast network of ITIs spread over various States / Union Territories in the country. The day-to-day administration of ITIs under the Craftsmen Training Scheme was transferred to te State Government / Union Territory administrations with effect from the year 1956. from 1st April 1969, the financial control of the Industrial training Institutes in the States as well as the union Territories was transferred to the respective State Governments / Union Territory. the financial assistance was granted to them in the form of bulk grant in consultation with the crstwhile Planning Commission and the Ministry of finance. Vocational Training is a concurrent subject of both Central and State Governments. the development of training schemes at National level, the evolution of policy, laying of training standard, norms, conducting of examinations, certification, etc. are the responsibilities og Central Governments / UT's. Presently, training courses under the Craftsmen Training Scheme are being effered through a network of 15,042 ITIs (Govt. 2,730 + Private 12,304) loacated all over the country with total trainees enrolled 22.02 lakhs (in trades of 1 year and 2 year durations) on NCVTMIS portal with an objective to provide skilled workforce to the industry in 13.0 NSQF compliant trades.
+    </p>
+    </div>
+
+
+	</div>
+</body>
+</html>
